62b5dac3cf1dd90fbcdf77129d8ff80ac0126431
Alex <50584983+AlexTanui@users.noreply.github.com>
2023-02-16 09:52:23 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+  def initialize(first_name, last_name)
+    super
+    @knowledge = []
+  end
 
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
